64b9d99026ff878e529444ddf26c000948e5e903
fatineferdous <128898989+fatineferdous@users.noreply.github.com>
2023-03-29 15:49:55 +0200

    Update Rejet.java

--- Rejet.java
+++ Rejet.java
@@ -5,60 +5,53 @@ import java.util.*;
  * 
  */
 public class Rejet {
-
-    /**
-     * Default constructor
-     */
-    public Rejet() {
+    
+    private float cout;
+    private float taux_Pollution;
+    
+    // Constructeur
+    public Rejet(float cout, float taux_Pollution) {
+        this.cout = cout;
+        this.taux_Pollution = taux_Pollution;
     }
-
-    /**
-     * 
-     */
-    private Float cout;
-
-    /**
-     * 
-     */
-    private Float taux_pollution;
-
-    /**
-     * 
-     */
-    public void envoyer_ds_site_enfouissement() {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
+    
+    // Getters et setters
     public float getCout() {
-        // TODO implement here
+        return cout;
+    }
+    
+    public void setCout(float cout) {
+        this.cout = cout;
+    }
+    
+    public float getTauxPollution() {
+        return taux_Pollution;
+    }
+    
+    public void setTauxPollution(float taux_Pollution) {
+        this.taux_Pollution = taux_Pollution;
+    }
+    
+    // Méthodes
+    public void envoyer_ds_site_enfouissement0() {
+        // TODO : implémenter la méthode pour envoyer le rejet dans le site d'enfouissement du quartier 
+    }
+    
+    public float getCout0() {
+        // TODO : implémenter la méthode pour récupérer le coût du rejet dans le quartier 
         return 0.0f;
     }
-
-    /**
-     * @return
-     */
-    public float setCout() {
-        // TODO implement here
+    
+    public void setCout0(float cout) {
+        // TODO : implémenter la méthode pour modifier le coût du rejet dans le quartier 
+    }
+    
+    public float getTauxPollution0() {
+        // TODO : implémenter la méthode pour récupérer le taux de pollution du rejet dans le quartier 
         return 0.0f;
     }
-
-    /**
-     * @return
-     */
-    public Float getTauxPollution() {
-        // TODO implement here
-        return null;
+    
+    public void setTauxPollution0(float taux_Pollution) {
+        // TODO : implémenter la méthode pour modifier le taux de pollution du rejet dans le quartier 
     }
-
-    /**
-     * @return
-     */
-    public Float setTauxPollution() {
-        // TODO implement here
-        return null;
-    }
-
 }
