65876d7fd7d59d379c1d6a4ba07217a1c67c3f63
Khadidiatou01 <128898922+Khadidiatou01@users.noreply.github.com>
2023-03-29 16:05:42 +0200

    Update Conditionneurs.java

--- Conditionneurs.java
+++ Conditionneurs.java
@@ -1,78 +1,75 @@
+package javaproject;
 
 import java.util.*;
 
-/**
- * 
- */
+public class Conditionneurs {
+/*attributs*/
+	private Integer id_conditionneurs;
+	private String nom;
+	
+/*getters et setters*/
+	public int getId_conditionneurs() {
+		return id_conditionneurs;
+	}
+	
+	public void setId_conditionneurs(int id_conditionneurs) {
+		this.id_conditionneurs = id_conditionneurs;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	} 
+	
+}
+
+
+/*
+
 public class Conditionneurs {
 
-    /**
-     * Default constructor
-     */
+    
     public Conditionneurs() {
     }
 
-    /**
-     * 
-     */
+   
     private Integer id_conditionneur;
 
-    /**
-     * 
-     */
+    
     private String nom;
 
-    /**
-     * @return
-     */
+    
     public Integer getIdConditionneur() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
     public String getNom() {
         // TODO implement here
         return "";
     }
 
-    /**
-     * 
-     */
+   
     public void preparer_dechet() {
-        // TODO implement here
+        
     }
 
-    /**
-     * 
-     */
+   
     public void laver() {
-        // TODO implement here
+       
     }
-
-    /**
-     * 
-     */
     public void decouper() {
-        // TODO implement here
+        
     }
-
-    /**
-     * @return
-     */
     public String setNom() {
-        // TODO implement here
         return "";
     }
-
-    /**
-     * @return
-     */
     public Integer setIdConditionneur() {
-        // TODO implement here
         return null;
     }
 
 }
+*/
