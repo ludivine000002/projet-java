c5cd7fe2c79c9c40544c0bd10a57453975be8b91
Khadidiatou01 <128898922+Khadidiatou01@users.noreply.github.com>
2023-03-29 16:06:35 +0200

    Update entrepriseOrganisations.java

--- entrepriseOrganisations.java
+++ entrepriseOrganisations.java
@@ -1,64 +1,69 @@
+package javaproject;
 
 import java.util.*;
 
-/**
- * 
- */
+public class entrepriseOrganisations {
+/*attributs*/
+	
+	private Integer id_entreprise;
+	private String nom;
+	
+/*getters et setters*/
+	
+	public Integer getId_entreprise() {
+		return id_entreprise;
+	}
+	public void setId_entreprise(Integer id_entreprise) {
+		this.id_entreprise = id_entreprise;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	} 
+	
+}
+
+/*
 public class entrepriseOrganisations {
 
-    /**
-     * Default constructor
-     */
     public entrepriseOrganisations() {
     }
 
-    /**
-     * 
-     */
+   
     private Integer id_entreprise;
 
-    /**
-     * 
-     */
+    
     private String nom;
 
-    /**
-     * 
-     */
+   
     public void integrer_dans_processus_production() {
         // TODO implement here
     }
 
-    /**
-     * @return
-     */
+    
     public string getNom() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
+    
     public string setNom() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
+   
     public integer getIdEtp() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
+   
     public integer setIdEtp() {
         // TODO implement here
         return null;
     }
 
-}
+}*/
