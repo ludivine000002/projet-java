b2d95ccc7d85e9d8e408022e64ea1725af150b5f
ludivine000002 <129295394+ludivine000002@users.noreply.github.com>
2023-03-29 16:06:52 +0200

    Update Dechets.java

--- Dechets.java
+++ Dechets.java
@@ -1,4 +1,86 @@
+import java.util.*;
 
+/*class dechets */
+public class Dechets {
+
+	/*constructeur de la classe déchet */
+    public Dechets(String type,Float poids) {
+    	this.type = type;
+    	this.poids = poids;
+    	
+    }
+
+  /*attributs de la classe déchet */
+    private String type;
+    private Float poids;
+
+    /*getter et setters*/
+    public Float getPoids() {
+        return null;
+    }
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setPoids(Float poids) {
+		this.poids = poids;
+	}
+
+}
+	
+	
+	// La classe Verre qui hérite de la classe Dechet
+	public class Verre extends Dechets {
+	
+		/*attribut*/
+		private Float pv;
+		
+	    // Constructeur
+	    public Verre(String type,Float poids,float pv) {
+	        super(type,poids);
+	        this.pv = pv;
+	    }
+
+	    /*getter et setter*/
+	    
+	    public float getPv() {
+	        return pv;
+	    }
+
+	    public void setPv(float pv) {
+	        this.pv = pv;
+	    }
+	
+	}
+	
+	// La classe Plastique qui hérite de la classe Dechet
+		public class Plastique extends Dechets {
+		
+			/*attribut*/
+			private Float pp;
+			
+		    // Constructeur
+		    public Plastique(String type,Float poids,float pp) {
+		        super(type,poids);
+		        this.pp = pp;
+		    }
+
+		    /*getter et setter*/
+		    
+		    public float getPp() {
+		        return pp;
+		    }
+
+		    public void setPv(float pp) {
+		        this.pp = pp;
+		    }
+		}
+		
 		// La classe Carton qui hérite de la classe Dechet
 				public class Carton extends Dechets {
 				
@@ -21,4 +103,26 @@
 				        this.pc = pc;
 				    }
 				}
+						
+				// La classe Metal qui hérite de la classe Dechet
+				public class Metal extends Dechets {
+				
+					/*attribut*/
+					private Float pm;
 					
+				    // Constructeur
+				    public Metal(String type,Float poids,float pm) {
+				        super(type,poids);
+				        this.pm = pm;
+				    }
+
+				    /*getter et setter*/
+				    
+				    public float Getpm() {
+				        return pm;
+				    }
+
+				    public void setPm(float pm) {
+				        this.pm = pm;
+				    }
+				}
